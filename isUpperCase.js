f6d1967ea72c70a66cd13ae90cc658080d24a2be
JasonCK <x13244231@gmail.com>
2021-12-30 01:53:16 +0800

    isUpperCase

---
+++ isUpperCase.js
@@ -0,0 +1,11 @@
+function isUpperCase(str) {
+    console.log(str[0]);
+
+    if(str[0] === str[0].toUpperCase()){
+        return console.log(true);
+    } else {
+        return console.log(false);
+    };
+};
+
+isUpperCase("abcd");

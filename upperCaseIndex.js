f6a91def9577d9a7238af2bee49595f0a2e585f7
JasonCK <x13244231@gmail.com>
2022-01-14 03:13:23 +0800

    Completed

--- upperCaseIndex.js
+++ upperCaseIndex.js
@@ -1,13 +1,16 @@
 function position(str) {
+
     for(var letterIndex = 0; letterIndex < str.length; letterIndex++) {
-        console.log(letterIndex)
-        
         if(str[letterIndex] === str[letterIndex].toUpperCase()) {
             return console.log(str[letterIndex], letterIndex)
-        } else {
-            return console.log(letterIndex);
         }
     }
+    return console.log("-1")
 }
 
-position("ABcd")
+position("Abcd")
+position("aBcd")
+position("abCd")
+position("abcD")
+position("abCD")
+position("abcd")

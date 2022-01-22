306485b2efa40f203f6fb4b98a64ecbccd0ecd3e
JasonCK <x13244231@gmail.com>
2022-01-23 04:00:50 +0800

    Finish

---
+++ table9to9.js
@@ -0,0 +1,12 @@
+function table9to9() {
+
+    for(var i = 1; i <= 9; i++) {
+        for(var n = 1; n <= 9; n++) {
+            console.log(`${i}*${n} =`, i*n)
+        }
+
+    }
+    return
+}
+
+table9to9()

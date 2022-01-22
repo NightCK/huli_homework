39add56402a3e8e153c6e0d7d4bc9b0c3ba786f3
JasonCK <x13244231@gmail.com>
2022-01-23 02:15:46 +0800

    Finish

--- stars2.js
+++ stars2.js
@@ -1,23 +1,22 @@
+// 範例：輸入 5，會得到 5 排星星，每一排星星數量和排數相同。
+// 到第 6 排開始，星星會以遞減方式，每一排減少一顆星星直到歸零。
+
+var result =""
+
 function stars2(n) {
-    var result = ""
-    
-    for(var line = 1; line <= n; line++) {
-        
-        if(line != n + 1){
-            for(var i = 0; i < line; i++) {
-                result += "*"
-            }
-        } else {
-            console.log("else")
-        }
-
-
-        if(line < n) {
+    for(var lineNumber = 1; lineNumber <= n; lineNumber++) {
+        result += "*".repeat(lineNumber)
+        if(lineNumber < n) {
             result += "\n"
         }
+    }
+
+    for(var i = n - 1; i >= 1; i-- ) {
+        result += "\n"
+        result += "*".repeat(i)
     }
 
     return result
 }
 
-console.log(stars2(3))
+console.log(stars2(5))

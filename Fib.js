a2bdef89919adbf863ed6ab42b74fc2cefc721ec
JasonCK <x13244231@gmail.com>
2022-01-28 23:27:32 +0800

    Finish

--- Fib.js
+++ Fib.js
@@ -3,13 +3,14 @@ function fib(n) {
 
     for(var i = 1; i <= n; i++ ) {
         if(i % 3 == 0){ 
-            fibArray.push(i)
-            console.log(i)
-        } else { 
             fibArray.push((i-1)+(i-2));
-            console.log(i)
+            console.log("if", i)
+        } else { 
+            fibArray.push(i)
+            console.log("else", i)
         }
     }
-    return fibArray
+
+    return fibArray[n-1]
 }
-console.log(fib(5))
+console.log(fib(2))

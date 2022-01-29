fe546a9589fdb3b50166a7973a02b4d00e6551e6
JasonCK <x13244231@gmail.com>
2022-01-29 20:37:07 +0800

    遞迴寫法

--- Fib.js
+++ Fib.js
@@ -1,16 +1,30 @@
-function fib(n) {
-    var fibArray = []
+// function fib(n) {
+//     var fibArray = []
 
-    for(var i = 1; i <= n; i++ ) {
-        if(i % 3 == 0){ 
-            fibArray.push((i-1)+(i-2));
-            console.log("if", i)
-        } else { 
-            fibArray.push(i)
-            console.log("else", i)
-        }
+//     for(var i = 1; i <= n; i++ ) {
+//         if(i % 3 == 0){ 
+//             fibArray.push((i-1)+(i-2));
+//         } else { 
+//             fibArray.push(i)
+//         }
+//     }
+
+//     return fibArray[n-1]
+// }
+// console.log(fib(2))
+
+// 遞迴的寫法
+
+function fibReverse(n) {
+    if(n == 1){
+      return 1;
     }
-
-    return fibArray[n-1]
-}
-console.log(fib(2))
+    else if(n == 2){
+      return 1;
+    }
+    else{
+      console.log(fibReverse(n-1))
+      console.log(fibReverse(n-2))
+      return fibReverse(n-1) + fibReverse(n-2);
+    }
+  }

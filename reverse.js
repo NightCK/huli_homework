8e7ef173760fb7d4541f2eedd79eaa4aa59d7322
JasonCK <x13244231@gmail.com>
2022-01-29 22:27:03 +0800

    省略部分的 code

--- reverse.js
+++ reverse.js
@@ -2,11 +2,7 @@ function reverse(str) {
     var strArray = str.split('')
 
     strArray.sort(function(a, b) {
-        if(strArray[a] < strArray[b]){ // 比較 index number，比較大的就會排前面
-            return 1
-        } else {
-            return -1
-        }
+      return -1
     })
 
     console.log(strArray.join(''))

0ec7cd1123fb8659d55c526e2a7532ba93a7c644
JasonCK <x13244231@gmail.com>
2022-03-06 03:17:51 +0800

    Completed

---
+++ findMin.js
@@ -0,0 +1,14 @@
+function findMin(arr) {
+    var min = arr[0];
+
+    for(var i = 1; i <= arr.length; i++) {
+        if(min > arr[i]) {
+            min = arr[i]
+        } ;
+    };
+
+    console.log("result is ", min)
+};
+
+findMin([1, 2, 5, 6, 99, 4, 5]);
+findMin([1, 6, 0, 33, 44, 88, -10])

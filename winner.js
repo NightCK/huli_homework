ca22ffad4f4069bd6f7a0107023b2293b8cab792
JasonCK <x13244231@gmail.com>
2022-04-03 17:11:55 +0800

    改成用位置判斷

--- winner.js
+++ winner.js
@@ -1,58 +1,57 @@
-// 本來是想盡辦法想去對比各種排列組合，
-// 後來拿起筆自己玩了幾場圈圈叉叉後，
-// 發現贏家其實就是棋子數量比較多的那邊，
-// 只看結果的話是成立的，
-// 但如果是動態的遊戲，還想不到用位置判斷以外的選擇。
-
 function winner(arr) {
-    var resultOfO = []
-    var resultOfX = []
-    var inputArray = arr.flat() // 壓平陣列，增加可讀性
-
-    // 判斷每個數字是 O 還是 X，如果是 O 就推到 resultOfO，是 X 就推到 resultOfX
-    inputArray.forEach(function(index) {
-            if( index === 'O' ) {
-                resultOfO.push('O')
-            } else if ( index === 'X') {
-                resultOfX.push('X')
-            }
+    for(var i = 0; i<arr.length; i++) {
+        if(arr[i][0] === arr[i][1] && arr[i][1] === arr[i][2] && arr[i][0] !== '') {
+            return arr[i][0]
         }
-    )
-    
-    // 比較 O 還是 X 的數量，多的就是贏家，一樣則是平手
-    if (resultOfO.length > resultOfX.length) {
-        return console.log('O')
-    } else if(resultOfX.length > resultOfO.length) {
-        return console.log('X')
-    } else if (resultOfX.length === resultOfO.length) {
-        return console.log('draw')
+
+        if(arr[0][i] === arr[1][i] && arr[1][i] === arr[2][i] && arr[0][i] !== '') {
+            return arr[0][i]
+        }
+
+        if(arr[0][0] === arr[1][1] && arr[1][1] === arr[2][2] && arr[0][0] !== '') {
+            return arr[0][0]
+        }
+
+        if(arr[0][2] === arr[1][1] && arr[1][1] === arr[2][0] && arr[0][2] !== '') {
+            return arr[0][2]
+        }
     }
+
+    return 'draw'
 }
 
-winner([
-    ['O', 'O', 'X'],
-    ['O', 'X', 'X'],
-    ['O', 'X', 'O']
-]) 
-// 預期回傳值：O
-    
-winner([
-    ['O', 'O', 'X'],
-    ['O', 'X', 'X'],
-    ['X', 'X', 'O']
-]) 
-// 預期回傳值：X
+console.log(
+    winner([
+        ['O', 'O', 'X'],
+        ['O', 'X', 'X'],
+        ['O', 'X', 'O']
+    ]) 
+    // 預期回傳值：O
+)
 
-winner([
-    ['O', 'O', 'X'],
-    ['O', 'O', 'X'],
-    ['X', 'X', '']
-])  
-// 預期回傳值：draw
-
-winner([
-    ['O', '', 'X'],
-    ['O', '', 'X'],
-    ['', '', 'X']
-])  
+console.log(
+    winner([
+        ['O', 'O', 'X'],
+        ['O', 'X', 'X'],
+        ['X', 'X', 'O']
+    ]) 
     // 預期回傳值：X
+)
+
+console.log(
+    winner([
+        ['O', 'O', 'X'],
+        ['O', 'O', 'X'],
+        ['X', 'X', '']
+    ])  
+    // 預期回傳值：draw
+)
+
+console.log(
+    winner([
+        ['O', '', 'X'],
+        ['O', '', 'X'],
+        ['', '', 'X']
+    ])  
+    // 預期回傳值：X
+)
